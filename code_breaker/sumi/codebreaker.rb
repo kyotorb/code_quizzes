63e0ff80562f3f87da6493a9107c4aa34d016414
kakusuke <bananafishmonger+github@gmail.com>
2013-11-07 20:50:00 +0900

    all test passed!

--- code_breaker/sumi/codebreaker.rb
+++ code_breaker/sumi/codebreaker.rb
@@ -1,11 +1,36 @@
 module Codebreaker
 	class Game
 		def initialize(output)
-			output.puts "Welcome to Codebreaker!"
-			output.puts "Enter guess: "
+			@output = output
 		end
 
-		def start(number)
+		def start(secret)
+			@secret = secret.each_char.to_a
+			@output.puts "Welcome to Codebreaker!"
+			@output.puts "Enter guess: "
+		end
+
+		def guess(numbers)
+			@numbers = numbers.each_char.to_a
+
+			'+' * hit_count + '-' * match_count
+		end
+
+	private
+		def total_count
+			(0..9).map(&:to_s).reduce(0) do |count, n|
+				count + [@secret.count(n), @numbers.count(n)].min
+			end
+		end
+
+		def hit_count
+			@secret.each_index.reduce(0) do |count, idx|
+				count + (@secret[idx] == @numbers[idx] ? 1 : 0)
+			end
+		end
+
+		def match_count
+			total_count - hit_count
 		end
 	end
 end
