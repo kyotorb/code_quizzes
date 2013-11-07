7bc57d79a98e8d0bbe53a6f1ba6626df6b3dccb0
kakusuke <bananafishmonger+github@gmail.com>
2013-11-07 20:36:24 +0900

    copy feature files

---
+++ code_breaker/sumi/features/codebreaker_starts_game.feature
@@ -0,0 +1,11 @@
+Feature: codebreaker starts game
+  As a codebreaker
+  I want to start a game
+  So that I can break the code
+
+  Scenario: start game
+  Given I am not yet playing
+  When I start a new game
+  Then I should see "Welcome to Codebreaker!"
+  And I should see "Enter guess: "
+  
---
+++ code_breaker/sumi/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,38 @@
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(output)
+  game.start('1234')
+end
+
+Then /^I should see "([^\"]*)"$/ do |message|
+  output.messages.should include(message)
+end
+
+Given /^the secret code is "([^\"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(output)
+  @game.start(secret)
+end
+
+When /^I guess "([^\"]*)"$/ do |guess|
+  @guess = guess
+end
+
+Then /^the mark should be "([^\"]*)"$/ do |mark|
+  @game.guess(@guess).should == mark
+end
+
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||= Output.new
+end
